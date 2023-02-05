e2f9be8312d33da477bccf3a8228287b5a02bdc9
Arjunan323 <uarjunan97@gmail.com>
2023-02-05 14:50:18 +0530

    different selectors. closes #2

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

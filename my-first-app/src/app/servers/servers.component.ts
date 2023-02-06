30de3ba1dd10612b1a434b29e95402e00af752ca
Arjunan323 <uarjunan97@gmail.com>
2023-02-07 00:07:31 +0530

    Directives. closes #7

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -14,6 +14,10 @@ export class ServersComponent {
   serverCreationStatus: string = 'No Server was created!'
   serverName: string = 'Test';
   username: string = '';
+  serverCreated:boolean = false
+  servers:Array<string> = ['Test Server' , "Test Server 2"   ]
+  showSecret:boolean = false;
+  log:Array<Date> = []
 
   constructor() {
     setTimeout(() => {
@@ -22,14 +26,24 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created!. Name is '+this.serverName;
   }
 
   onUpdateServerName(event: Event) {
+    this.serverCreated = false;
     this.serverName = (<HTMLInputElement> event.target ).value
   }
 
   onRestUser(){
     this.username = ''
+  }
+
+  
+  onToggleDetails(){
+      this.showSecret = !this.showSecret
+      // this.log.push(this.log.length + 1)
+      this.log.push(new Date())
   }
 }

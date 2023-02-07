823966c58252a7e412e9028c2c929642560fa9f5
Arjunan323 <uarjunan97@gmail.com>
2023-02-07 19:56:22 +0530

    UI for Recipe ,  Details component. closes #10

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes:Array<Recipe> = [
+    new Recipe('Test Recipe', 'Test' , 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg')
+  ]
 }

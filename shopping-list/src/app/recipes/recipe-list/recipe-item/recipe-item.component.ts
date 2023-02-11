0c01711810cdbbac09a352acf08a4b836461c97a
Arjunan323 <uarjunan97@gmail.com>
2023-02-12 00:33:59 +0530

    Event Binding and Property Binding. closes #18

--- shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input , EventEmitter , Output} from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent{
+  @Input() recipe:Recipe ;
+  @Output() recipeSelected = new EventEmitter();
 
+
+  constructor(){
+
+  }
+
+  onSelected(){
+    this.recipeSelected.emit()
+  }
 }
--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , EventEmitter , Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Array<Recipe> = [
-    new Recipe('Test Recipe', 'Test' , 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg')
+    new Recipe('Test Recipe', 'Test' , 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg'),
+    new Recipe('Test Recipe1', 'Test1' , 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg')
   ]
+
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }

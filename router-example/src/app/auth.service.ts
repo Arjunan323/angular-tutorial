cf465bac4d286ff5117acfefdd48c5c610d137ec
Arjunan323 <uarjunan97@gmail.com>
2023-04-07 00:14:03 +0530

    router sample project. closes #23

---
+++ router-example/src/app/auth.service.ts
@@ -0,0 +1,25 @@
+export class AuthService {
+
+    loggedIn: boolean = false;
+
+
+    isAuthenticated()  : Promise<boolean> {
+        const promise: Promise<boolean> = new Promise(
+            (resolve , reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn)
+                }, 800);
+            }
+        );
+
+        return promise;
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
